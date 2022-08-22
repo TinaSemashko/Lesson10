3c6200b55671dd7ec810f92d2cb4f43d7135ce69
TinaSemashko <akateria.satory@gmail.com>
2022-08-22 16:34:14 +0200

    Add project files.

---
+++ Lesson10/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public enum Conditions
+{
+    newBook,
+    good,
+    bad,
+    writeOff
+}
+
+public enum Statuses
+{
+    onShelf,
+    atTheReader,
+    lost
+}
+
+public class bookLibrary
+{
+    public static int maxAmount = 50000;
+    public string nameBook;
+    public string codeBook;
+    public Statuses statusBook;
+    public Conditions bookCondition;
+
+    public string getBookCode()
+    {       
+        return codeBook;
+    }
+
+    public string getBookName()
+    {
+        return nameBook;
+    }
+
+    public void setStatus(Statuses status)
+    {
+        statusBook = status;
+    }
+
+    public Statuses getStatus()
+    {
+        return statusBook;
+    }
+
+    public void setBookCondition(Conditions condition)
+    {
+        bookCondition = condition;
+    }
+    public Conditions getBookCondition()
+    {
+        return bookCondition;
+    }
+}
